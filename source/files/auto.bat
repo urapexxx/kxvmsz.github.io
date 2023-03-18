5be8a6e78eb26ed65806d6228bfba62ffa2085e9
urapexxx <128257331+urapexxx@users.noreply.github.com>
2023-03-18 23:06:00 +0300

    Add files via upload

---
+++ source/files/auto.bat
@@ -0,0 +1,4 @@
+@echo off
+start "Fortnite" "D:\fortnite\Fortnite\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe"
+start "TimerResolution" "G:/TimerResolution.exe"
+start "wooting-double-movement.exe" "C:\Users\anski\AppData\Local\Programs\wooting-double-movement\wooting-double-movement.exe"
